5dde767d66e37a9214a7cb5e0ee0f23b685d3b22
Ankit Pandey <ankitwebs@gmail.com>
2018-07-19 17:19:22 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,16 +1,16 @@
-add_action( 'init', 'droid_buttons' );
-function droid_buttons() {
-    add_filter( "mce_external_plugins", "droid_add_buttons" );
-    add_filter( 'mce_buttons', 'droid_register_buttons' );
+add_action( 'init', 'new_tab_buttons' );
+function new_tab_buttons() {
+    add_filter( "mce_external_plugins", "new_tab_add_buttons" );
+    add_filter( 'mce_buttons', 'new_tab_register_buttons' );
 }
 
-function droid_add_buttons( $plugin_array ) {
-    $plugin_array['droid'] = get_template_directory_uri() . '/text-button.js';
+function new_tab_add_buttons( $plugin_array ) {
+    $plugin_array['new_tab'] = get_template_directory_uri() . '/js/text-button.js';
     echo get_template_directory_uri();
     return $plugin_array;
 }
 
-function droid_register_buttons( $buttons ) {
-    array_push( $buttons, 'droid_title'); // droid_title
+function new_tab_register_buttons( $buttons ) {
+    array_push( $buttons, 'new_tab_title'); // 
     return $buttons;
 }
